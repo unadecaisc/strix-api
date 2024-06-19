0c11fdaa0a66f2e9a31a45ba5f48ee67ddaa78fb
Wilian Martinez <wmartzh@gmail.com>
2024-06-19 16:24:08 -0600

    Feature/2q24 14 firebase auth (#3)
    * feat(auth):add prisma migrations
    
    add user service
    
    * feat(config): add  firebase
    
    * feat(config): add firebase service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { FireabaseService } from './fireabase/fireabase.service';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FireabaseService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as firebaseAdmin from 'firebase-admin';
+import { config } from 'dotenv';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { getCredentialsFromEnv } from './utils';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -12,6 +17,13 @@ async function bootstrap() {
       logger: true,
     }),
   );
+
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(getCredentialsFromEnv()),
+  });
+
+  app.enableCors();
+
   await app.listen(process.env.PORT || 8000, '0.0.0.0');
 }
 bootstrap();
