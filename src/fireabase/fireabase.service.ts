81c99d19b8cb0ca06d4f173a5bf861ca5d5f3efb
Wilian Martinez <wmartzh@gmail.com>
2024-06-19 16:22:47 -0600

    feat(config): add firebase service

---
+++ src/fireabase/fireabase.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+
+export type FirebaseUser = {
+  email: string;
+  password: string;
+  displayName: string;
+};
+
+@Injectable()
+export class FireabaseService {
+  validateToken(token: string) {
+    return admin.auth().verifyIdToken(token);
+  }
+
+  createUser(user: FirebaseUser) {
+    return admin.auth().createUser(user);
+  }
+
+  getUser(uid: string) {
+    return admin.auth().getUser(uid);
+  }
+
+  updateUser(uid: string, user: FirebaseUser) {
+    return admin.auth().updateUser(uid, user);
+  }
+
+  deleteUser(uid: string) {
+    return admin.auth().deleteUser(uid);
+  }
+}
