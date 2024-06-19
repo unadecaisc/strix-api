702b8def930235ab8a254c8c1bfeba66813373f0
Wilian Martinez <wmartzh@gmail.com>
2024-06-19 16:17:24 -0600

    feat(config): add  firebase

---
+++ src/utils/credentials.util.ts
@@ -0,0 +1,11 @@
+export function getCredentialsFromEnv() {
+  if (process.env.FIREBASE_CREDENTIAL) {
+    const base = Buffer.from(
+      process.env.FIREBASE_CREDENTIAL,
+      'base64',
+    ).toString('utf-8');
+
+    const obj = JSON.parse(base);
+    return obj;
+  }
+}
