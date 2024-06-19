1bf76e61914e9bfc4afbe1c51b24e8b3d7b2eff3
Wilian Martinez <wmartzh@gmail.com>
2024-06-19 17:47:37 -0600

    feat(users): Add user service and controller
    swagger added

---
+++ src/users/users.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from '@prisma/client';
+import { ApiTags } from '@nestjs/swagger';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@ApiTags('users')
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':uuid')
+  async findOne(@Param('uuid') uuid: string): Promise<User> {
+    return this.usersService.findOne(uuid);
+  }
+
+  @Post()
+  async create(@Body() body: CreateUserDto): Promise<User> {
+    return this.usersService.create(body);
+  }
+
+  @Put(':uuid')
+  async update(
+    @Body() body: UpdateUserDto,
+    @Param('uuid') uuid: string,
+  ): Promise<User> {
+    return this.usersService.update(uuid, body);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,127 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from '../common/prisma.service';
+import { FirebaseService } from '../common/fireabase.service';
+import { UserRecord } from 'firebase-admin/lib/auth/user-record';
+import { Prisma, User } from '@prisma/client';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+const DEFAULT_ROLE = 2;
+
+@Injectable()
+export class UsersService {
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly firebaseService: FirebaseService,
+  ) {}
+
+  createUserInput(
+    createUserDto: CreateUserDto,
+    firebaseUser: UserRecord,
+  ): Prisma.UserCreateInput {
+    const input: Prisma.UserCreateInput = {
+      email: createUserDto.email,
+      name: createUserDto.name,
+      phone: createUserDto.phone,
+      uuid: firebaseUser.uid,
+      role: {
+        connect: { id: DEFAULT_ROLE },
+      },
+    };
+
+    if (createUserDto.roleId) {
+      input.role = {
+        connect: { id: createUserDto.roleId },
+      };
+    }
+
+    if (createUserDto.departmentId) {
+      input.department = {
+        connect: { id: createUserDto.departmentId },
+      };
+    }
+
+    return input;
+  }
+
+  createUpdateUserInput(updateUser: UpdateUserDto): Prisma.UserUpdateInput {
+    const input: Prisma.UserUpdateInput = {
+      ...updateUser,
+    };
+    return input;
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    if (await this.exists(createUserDto.email)) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const firebaseUser = await this.firebaseService.createUser({
+      email: createUserDto.email,
+      password: createUserDto.password,
+    });
+
+    const user = await this.prismaService.user.create({
+      data: this.createUserInput(createUserDto, firebaseUser),
+    });
+
+    return user;
+  }
+
+  findAll() {
+    return this.prismaService.user.findMany({
+      include: {
+        role: true,
+        department: true,
+      },
+    });
+  }
+
+  private async exists(email: string): Promise<boolean> {
+    try {
+      const user = await this.prismaService.user.findFirst({
+        where: {
+          email,
+        },
+      });
+
+      return !!user;
+    } catch (error) {
+      return false;
+    }
+  }
+  findOne(uuid: string) {
+    return this.prismaService.user.findFirst({
+      where: {
+        uuid,
+      },
+      include: {
+        role: true,
+        department: true,
+      },
+    });
+  }
+
+  async update(uuid: string, updateUserDto: UpdateUserDto): Promise<User> {
+    // return `This action updates a #${id} user`;
+    //update firebase
+
+    if (updateUserDto || updateUserDto.password) {
+      this.firebaseService.updateUser(uuid, {
+        password: updateUserDto.password,
+        email: updateUserDto.email,
+      });
+    }
+    //update prisma
+    const user = await this.prismaService.user.update({
+      where: { uuid },
+      data: this.createUpdateUserInput(updateUserDto),
+    });
+
+    return user;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
