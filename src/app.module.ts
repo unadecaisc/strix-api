652b0a325b3ac88429db3e4d907c68953131ee01
Wilian Martinez <wmartzh@gmail.com>
2024-06-25 17:01:38 -0600

    refactor(auth): remove middleware setup
    to be used in the future

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,11 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
-import { AuthMiddleware } from './auth/auth.middleware';
-
 @Module({
   imports: [CommonModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
