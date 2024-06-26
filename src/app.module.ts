e79b1de30b9179814fac2001cca8c3302c915e5c
jake <jake.corrales@unadeca.net>
2024-06-26 15:58:53 -0600

    refactor(settings): conflicting files resolved

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
+import { RolesModule } from './roles/roles.module';
 import { PeriodsModule } from './periods/periods.module';
 
 @Module({
-  imports: [CommonModule, UsersModule, PeriodsModule],
+  imports: [CommonModule, UsersModule,RolesModule, PeriodsModule],
   controllers: [AppController],
   providers: [AppService],
 })
