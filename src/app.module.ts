b23622793b89759b3e07f871d185b230a83a2527
jake <jake.corrales@unadeca.net>
2024-06-25 19:17:01 -0600

    feat(settings): add resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
+import { PeriodsModule } from './periods/periods.module';
 
 @Module({
-  imports: [CommonModule, UsersModule],
+  imports: [CommonModule, UsersModule, PeriodsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/periods/periods.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { PeriodsService } from './periods.service';
+import { CreatePeriodDto } from './dto/create-period.dto';
+import { UpdatePeriodDto } from './dto/update-period.dto';
+
+@Controller('periods')
+export class PeriodsController {
+  constructor(private readonly periodsService: PeriodsService) {}
+
+  @Post()
+  create(@Body() createPeriodDto: CreatePeriodDto) {
+    return this.periodsService.create(createPeriodDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.periodsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.periodsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePeriodDto: UpdatePeriodDto) {
+    return this.periodsService.update(+id, updatePeriodDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.periodsService.remove(+id);
+  }
+}
