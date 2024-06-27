4ceb4f74f2b94f2beb2d43ebb9c768b8f1a33fcd
Wilian Martinez <wmartzh@gmail.com>
2024-06-26 21:13:22 -0600

    refactor(periods): fix paths

--- src/periods/periods.controller.ts
+++ src/periods/periods.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Param, Query, Put } from '@nestjs/common';
 import { PeriodsService } from './periods.service';
 import { CreatePeriodDto } from './dto/create-period.dto';
 import { UpdatePeriodDto } from './dto/update-period.dto';
-import { GetPeriodDto } from './dto/get-Period.dto';
+import { GetPeriodDto } from './dto/get-period.dto';
 import { Period } from '@prisma/client';
 import { PaginatedResponse } from '../utils/pagination.util';
 
